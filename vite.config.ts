f7e57d4be8c019d2c4d06810000781c6b6d44fde
texaaiadm <texa.ai.adm@gmail.com>
2026-02-14 00:44:30 +0700

    chore: trigger redeploy for new profile

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Trigger redeploy for user profile switch
 export default defineConfig(({ mode }) => {
+
   const env = loadEnv(mode, '.', '');
   return {
     server: {
